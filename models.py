29861357862461b129e28cc2c47b36042732b024
yamanalkahwaji <yamanalkahwaji@std.sehir.edu.tr>
2016-11-24 21:01:41 +0300

    "software assignment 1"

---
+++ models.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+
+myCourses={1:('CS 361','Software'),2:('CS 352','Computer Architecture'),3:('CS 371','Data Structures and Algorithms'),4:('CS 200','Summer Internship'),5:('MGT 416','Psychology of Work and Career'),6:('UNI 201','Formations Of Moder Turkey I'),7:('POLS 207','Fundmentals Of Law')}
